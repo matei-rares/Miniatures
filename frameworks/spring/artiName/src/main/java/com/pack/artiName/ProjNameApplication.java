ddadaa80a94349a0f5b5ccf80bb11884350d0814
Matei Rares (uih01717) <rares.2.matei@continental-corporation.com>
2025-09-17 16:19:41 +0300

    init

---
+++ frameworks/spring/artiName/src/main/java/com/pack/artiName/ProjNameApplication.java
@@ -0,0 +1,15 @@
+package com.pack.artiName;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjNameApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(ProjNameApplication.class, args);
+        System.out.println("-----------------------------------START------------");
+    }
+
+}
---
+++ frameworks/spring/artiName/src/test/java/com/pack/artiName/ProjNameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pack.artiName;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjNameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
